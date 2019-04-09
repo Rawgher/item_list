b71d45fc1ef7e74266f61a3c429cd4f167bab012
Roger <rdpender3@gmail.com>
2019-04-08 21:52:15 -0700

    setting up for firebase

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import React, { Component } from "react";
-import { Platform, View } from "react-native";
+import { ListView, TouchableHighlight, View } from "react-native";
 import Toolbar from "./components/Toolbar/Toolbar";
-const styles = "./components/style.js";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+const styles = require("./components/style.js");
 
 export default class App extends Component {
+  constructor() {
+    super();
+    let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      itemDataSource: ds
+    };
+
+    this.renderRow = this.renderRow.bind(this);
+    this.pressRow = this.pressRow.bind(this);
+  }
+
+  componentWillMount() {
+    this.getItems;
+  }
+
+  componentDidMount() {
+    this.getItems;
+  }
+
+  getItems() {
+    let items = [{ title: "Item One" }, { title: "Item Two" }];
+
+    this.setState({
+      itemDataSource: this.state.itemDataSource.cloneWithRows(items)
+    });
+  }
+
+  pressRow(item) {
+    console.log(item);
+  }
+
+  renderRow(item) {
+    return (
+      <TouchableHighlight
+        onPress={() => {
+          this.pressRow(item);
+        }}
+      >
+        <View style={styles.li}>
+          <Text style={styles.liText}>{item.title}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
   render() {
     return (
       <View>
         <Toolbar title="ItemLister" />
+        <ListView
+          dataSource={this.state.itemDataSource}
+          renderRow={this.renderRow}
+        />
       </View>
     );
   }
